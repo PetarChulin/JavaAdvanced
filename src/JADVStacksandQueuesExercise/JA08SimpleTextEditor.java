50ae3244e82b79a9eaa4184d7d7d020d5cf712ae
PetarChulin <petercbg@mail.bg>
2021-12-16 16:38:20 +0200

    Java Advanced

---
+++ src/JADVStacksandQueuesExercise/JA08SimpleTextEditor.java
@@ -0,0 +1,46 @@
+package JADVStacksandQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class JA08SimpleTextEditor {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int operations = Integer.parseInt(s.nextLine());
+
+        StringBuilder text = new StringBuilder();
+        ArrayDeque<String> undoList = new ArrayDeque();
+        for (int i = 0; i < operations; i++) {
+            String input = s.nextLine();
+            String command = input.split(" ")[0];
+            switch (command) {
+
+                case "1":
+                    undoList.push(text.toString());
+                    String letter = input.split(" ")[1];
+                    text.append(letter);
+                    break;
+                case "2":
+                    undoList.push(text.toString());
+                    int count = Integer.parseInt(input.split(" ")[1]);
+                    while (count > 0) {
+                        text.deleteCharAt(text.length() - 1);
+                        count--;
+                    }
+                    break;
+                case "3":
+                    int index = Integer.parseInt(input.split(" ")[1]);
+                    System.out.println(text.charAt(index - 1));
+                    break;
+                case "4":
+                    assert undoList.peek() != null;
+                    text = new StringBuilder(undoList.peek());
+                    undoList.pop();
+                    break;
+            }
+        }
+    }
+}
