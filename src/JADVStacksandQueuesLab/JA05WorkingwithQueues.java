b734b4396341764fd7d3d00c77ce8424e6b7f53f
PetarChulin <petercbg@mail.bg>
2021-12-20 14:26:01 +0200

    Java Advanced

---
+++ src/JADVStacksandQueuesLab/JA05WorkingwithQueues.java
@@ -0,0 +1,33 @@
+package JADVStacksandQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class JA05WorkingwithQueues {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String input = s.nextLine();
+
+        ArrayDeque<String> lines = new ArrayDeque<>();
+
+        while (!input.equals("print")) {
+
+            if (input.equals("cancel")) {
+                if (lines.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + lines.pollFirst());
+                }
+            } else {
+                lines.offer(input);
+            }
+            input = s.nextLine();
+        }
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
+
