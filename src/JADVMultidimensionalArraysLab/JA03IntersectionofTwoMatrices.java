2f62d603e771c66b131cb98818ac0926d4e67f40
PetarChulin <petercbg@mail.bg>
2021-12-21 16:20:31 +0200

    Java Advanced

---
+++ src/JADVMultidimensionalArraysLab/JA03IntersectionofTwoMatrices.java
@@ -0,0 +1,41 @@
+package JADVMultidimensionalArraysLab;
+
+import java.util.Scanner;
+
+public class JA03IntersectionofTwoMatrices {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int rows = Integer.parseInt(s.nextLine());
+        int columns = Integer.parseInt(s.nextLine());
+
+        char[][] matrixOne = new char[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            String line = s.nextLine().replaceAll(" ", "");
+            char[] c = line.toCharArray();
+
+            if (columns >= 0) System.arraycopy(c, 0, matrixOne[i], 0, columns);
+        }
+        char[][] matrixTwo = new char[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            String line = s.nextLine().replaceAll(" ", "");
+            char[] c = line.toCharArray();
+
+            if (columns >= 0) System.arraycopy(c, 0, matrixTwo[i], 0, columns);
+        }
+        for (int i = 0; i < matrixOne.length; i++) {
+            for (int j = 0; j < matrixTwo[i].length; j++) {
+                if (matrixOne[i][j] == matrixTwo[i][j]) {
+                    System.out.print(matrixOne[i][j] + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+        System.out.println();
+        }
+    }
+}
+
