32e557668bff4ffc1aa8665089bf63cc0def9660
PetarChulin <petercbg@mail.bg>
2021-12-22 10:15:05 +0200

    Java Advanced

---
+++ src/JADVMultidimensionalArraysLab/JA04SumMatrixElements.java
@@ -0,0 +1,29 @@
+package JADVMultidimensionalArraysLab;
+
+import java.util.Scanner;
+
+public class JA04SumMatrixElements {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String[] dimensions = s.nextLine().split(",\\s+");
+        int rows = Integer.parseInt(dimensions[0]);
+        int columns = Integer.parseInt(dimensions[1]);
+        int[][] matrix = new int[rows][columns];
+        int sumElements = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            String[] elements = s.nextLine().split(",\\s+");
+            for (int j = 0; j < columns; j++) {
+
+                matrix[i][j] = Integer.parseInt(elements[j]);
+                sumElements += Integer.parseInt(elements[j]);
+//                sumElements += matrix[i][j];
+            }
+        }
+        System.out.printf("%d\n%d\n%d" , rows, columns, sumElements);
+    }
+}
+
