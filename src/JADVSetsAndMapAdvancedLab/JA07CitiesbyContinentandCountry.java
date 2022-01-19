a6d49a4aecbf5443b5c2da457ec893f3d50d1341
PetarChulin <petercbg@mail.bg>
2022-01-19 10:49:48 +0200

    Java Advanced

--- src/JADVSetsAndMapAdvancedLab/JA07CitiesbyContinentandCountry.java
+++ src/JADVSetsAndMapAdvancedLab/JA07CitiesbyContinentandCountry.java
@@ -1,8 +1,6 @@
 package JADVSetsAndMapAdvancedLab;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class JA07CitiesbyContinentandCountry {
     public static void main(String[] args) {
@@ -10,7 +8,7 @@ public class JA07CitiesbyContinentandCountry {
         Scanner s = new Scanner(System.in);
 
         int n = Integer.parseInt(s.nextLine());
-        LinkedHashMap<String, Map<String, String>> list = new LinkedHashMap<>();
+        LinkedHashMap<String, Map<String, List<String>>> list = new LinkedHashMap<>();
 
         while (n-- > 0) {
 
@@ -18,12 +16,27 @@ public class JA07CitiesbyContinentandCountry {
             String continent = input.split(" ")[0];
             String country = input.split(" ")[1];
             String city = input.split(" ")[2];
-            LinkedHashMap<String, String> listOfCountries = new LinkedHashMap<>();
-            listOfCountries.put(country , city);
-            if (!list.containsKey(continent)) {
-                list.put(continent, listOfCountries);
-            } else {
-                list.get(continent).put(country, city);
+
+            list.putIfAbsent(continent, new LinkedHashMap<>());
+            list.get(continent).putIfAbsent(country, new ArrayList<>());
+            list.get(continent).get(country).add(city);
+
+        }
+        printResult(list);
+//        list.forEach((key, value) -> {
+//            System.out.println(key + ":");
+//            value.forEach((k, v) ->
+//                    System.out.printf("%s -> %s%n", k, String.join(", ", v)));
+//        });
+    }
+
+    private static void printResult(LinkedHashMap<String, Map<String, List<String>>> list) {
+        for (Map.Entry<String, Map<String, List<String>>> entry : list.entrySet()) {
+            System.out.printf("%s: \n", entry.getKey());
+            for (Map.Entry<String, List<String>> country : entry.getValue().entrySet()) {
+                System.out.printf("  %s -> ", country.getKey());
+                System.out.print(String.join(", ", country.getValue()));
+                System.out.println();
             }
         }
     }
